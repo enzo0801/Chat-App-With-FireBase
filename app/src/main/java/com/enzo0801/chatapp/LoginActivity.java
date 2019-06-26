e735ba31cb946821dc7d20acc3324270c1e19c61
Enzo0801 <vodung0801@gmail.com>
2019-06-26 17:20:16 +0700

    .

--- app/src/main/java/com/enzo0801/chatapp/LoginActivity.java
+++ app/src/main/java/com/enzo0801/chatapp/LoginActivity.java
@@ -1,9 +1,9 @@
 package com.enzo0801.chatapp;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
+import androidx.appcompat.widget.Toolbar;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
@@ -14,8 +14,6 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.rengwuxian.materialedittext.MaterialEditText;
-
-import org.w3c.dom.Text;
 
 import androidx.annotation.NonNull;
 
